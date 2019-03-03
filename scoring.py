806d51c20240f7693499519e328e7dea006a7471
tedklin <tedklin@gmail.com>
2019-03-03 12:07:25 -0800

    fix scoring add up

--- scoring.py
+++ scoring.py
@@ -3,9 +3,9 @@ import math
 
 tba = tbapy.TBA('FsRCtOkFxHEy0mshFOrOXEfru18CkuRl4iKY4XFUsCM12w1BLawDoDVNrTWLHq6N')
 
-week = 6
-year = 2017
-team_list = [4613, 558, 1885, 1305, 503, 68]
+week = 1
+year = 2019
+team_list = [330, 1023, 1369, 319, 1102, 1318]
 
 
 def getScoreFromRank(rank):
@@ -42,22 +42,31 @@ def getScoreFromAward(award_type):
 	score = 0;
 	if (award_type == 0):
 		score = 42  # chairmans
+		print("Award: Chairmans | Points: " + str(42))
 	elif (award_type == 3):
 		score = 8  # woodie flowers
+		print("Award: Woodie Flowers | Points: " + str(8))
 	elif (award_type == 9):
 		score = 36  # engineering inspiration
+		print("Award: Engineering Inspiration | Points: " + str(36))
 	elif (award_type == 10):
 		score = 20  # rookie all-star
+		print("Award: Rookie All-Star | Points: " + str(20))
 	elif (award_type == 15):
 		score = 15  # rookie inspiration
+		print("Award: Rookie Inspiration | Points: ") + str(15)
 	elif (award_type == 14):
 		score = 5  # highest rookie seed
+		print("Award: Highest Rookie Seed | Points: " + str(5))
 	elif (award_type == 13):
-		score = 5  # judges
+		score = 5  # 
+		print("Award: Judges | Points: " + str(5))
 	elif (award_type == 16 or award_type == 17 or award_type == 20 or award_type == 21 or award_type == 29 or award_type == 71):
 		score = 15  # robot-based awards
+		print("Award: Robot-based awards | Points: " + str(15))
 	elif (award_type == 4):
 		score = 4  # deans list
+		print("Award: Deans List | Points: " + str(4))
 	return score
 
 total_ff_score = 0
@@ -93,8 +102,8 @@ for team in team_list:
 					# print(recipient["team_key"])
 					if (recipient["team_key"] == team_key):
 						award_type = award["award_type"]
-						print("Points from awards: " + str(getScoreFromAward(award_type)))
-						team_ff_score += getScoreFromElimResults(team_status)
+						# print("Points from awards: " + str(getScoreFromAward(award_type)))
+						team_ff_score += getScoreFromAward(award_type)
 
 			print("FF SCORE: " + str(team_ff_score))
 			print()
